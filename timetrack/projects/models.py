d928c5f29ed27d832661d9d77dceaeaf4af7c79a
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-10-07 11:01:46 +0200

    added default value for ttl_logged_tie and implemented save method of TimeLogEntry

--- timetrack/projects/models.py
+++ timetrack/projects/models.py
@@ -1,8 +1,13 @@
+from datetime import datetime, time
+
+
 from django.db import models
-from core.models import TimeStampedModel
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 
+from model_utils import FieldTracker
+
+from core.models import TimeStampedModel
 # Create your models here.
 User = get_user_model()
 
@@ -61,4 +66,31 @@ class TimeLogEntry(TimeStampedModel):
     paused = models.BooleanField(default=False)
     log_start_time = models.DateTimeField(
         null=True, blank=True, help_text=_("Log started time"))
-    ttl_logged_time = models.TimeField()
+    ttl_logged_time = models.TimeField(default=time(0, 0, 0))
+
+    # define trackers
+    tracker = FieldTracker(fields=['paused', 'completed'])
+
+    def save(self, *args, **kwargs):
+        _is_created = bool(self.pk is None)
+        _is_paused_changed = self.tracker.has_changed('paused')
+        _is_completed_changed = self.tracker.has_changed('completed')
+        _is_started = _is_created or (_is_paused_changed and not self.paused)
+        _is_paused = _is_paused_changed and self.paused and not self.completed
+        _is_completed = (_is_completed_changed and self.completed)
+
+        if _is_started:
+            # set log start time
+            self.log_start_time = datetime.now()
+
+            # if time track is started again, then stop other running trackers for current user
+            self.objects.filter(user=self.user, completed=False,
+                                paused=False).update(paused=True)
+
+        # if time track log is paused or completed, then update ttl_logged_time
+        if _is_paused or _is_completed:
+            # add ttl_logged_time
+            _time_elapsed = datetime.now() - self.log_start_time
+            self.ttl_logged_time = self.ttl_logged_time + _time_elapsed
+
+        super().save(*args, **kwargs)
